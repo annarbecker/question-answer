520f3a8b21322eb14738d45be2c1d68019fb50b1
Anna Becker <anna.r.becker@gmail.com>
2016-04-07 19:29:50 -0700

    Update total computed property to show link to questions if no questions are in favorite list, or display number of favorited questions

--- app/components/display-favorites.js
+++ app/components/display-favorites.js
@@ -2,15 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoritesList: Ember.inject.service(),
-  listIsShowing: false,
   actions: {
-    showList: function() {
-      this.set('listIsShowing', true);
-    },
-    hideList: function() {
-      this.set('listIsShowing', false);
-    },
-    removeFromList(question) {
+    removeFromFavorites(question) {
       this.get('favoritesList').remove(question);
     },
     emptyList() {
@@ -20,5 +13,10 @@ export default Ember.Component.extend({
   total: Ember.computed('favoritesList.questions.[]', function() {
     var listTotal = 0;
     listTotal += this.get('favoritesList.questions.length');
+    if(listTotal === 0) {
+      return Ember.String.htmlSafe("<a href='/'>Add Favorites</a>");
+    } else {
+      return Ember.String.htmlSafe("<p>Number of Favorites: " + listTotal + "</p>");
+    }
   })
 });
