96fd78ed5f1e052bc81605f02a684efb0501e4b2
Anna Becker <anna.r.becker@gmail.com>
2016-04-07 22:35:31 -0700

    Add route for each category to filter questions

---
+++ tests/unit/routes/tech-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:tech', 'Unit | Route | tech', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
