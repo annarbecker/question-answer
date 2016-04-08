7e5e2ab4c5f9ff6234468675a10bfe8d084ce681
Anna Becker <anna.r.becker@gmail.com>
2016-04-07 19:03:01 -0700

    Add button with addToList action to question-detail to allow user to add a question to their favorites list

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
   actions: {
     delete(question) {
       if(confirm('Are you sure you want to delete this question?')) {
        this.sendAction('delete', question);
      }
+   },
+   addToFavorites(question) {
+     this.get('favoritesList').add(question);
    }
  }
 });
