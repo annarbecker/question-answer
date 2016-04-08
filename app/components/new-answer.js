bd4c1a68453a5dfa0bbaca69bef98291f487e2f5
Anna Becker <anna.r.becker@gmail.com>
2016-04-08 16:42:00 -0700

    Fix bug with new answer save

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -3,16 +3,18 @@ import moment from 'moment';
 
 export default Ember.Component.extend({
   actions: {
-      saveAnswer() {
-        var params = {
-          author: this.get('author'),
-          response: this.get('response'),
-          question: this.get('question'),
-          timestamp: this.get('timestamp') ? this.get('timestamp') : moment().format('MMMM Do YYYY, h:mm a')
-        };
-        this.sendAction('saveAnswer', params);
-        this.set('author', '');
-        this.set('response', '');
-      }
+    saveAnswer() {
+      var params = {
+        author: this.get('author'),
+        response: this.get('response'),
+        question: this.get('question'),
+        timestamp: this.get('timestamp') ? this.get('timestamp') : moment().format('MMMM Do YYYY, h:mm a'),
+        upVotes: this.get('upVotes') ? this.get('upVotes') : '',
+        downVotes: this.get('downVotes') ? this.get('downVotes') : ''
+      };
+      this.sendAction('saveAnswer', params);
+      this.set('author', '');
+      this.set('response', '');
     }
+  }
   });
--- app/routes/question.js
+++ app/routes/question.js
@@ -29,7 +29,6 @@ export default Ember.Route.extend({
       newAnswer.save().then(function() {
         return question.save();
       });
-      this.transitionTo('question', params.question);
     },
     deleteAnswer(answer) {
       answer.destroyRecord();
