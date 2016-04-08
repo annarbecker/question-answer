ee7e624345c7dd6c563b7add47367300f4c41178
Anna Becker <anna.r.becker@gmail.com>
2016-04-07 18:30:52 -0700

    Display timestamp for when question was posted

--- app/components/new-question-form.js
+++ app/components/new-question-form.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
   actions: {
@@ -6,7 +7,8 @@ export default Ember.Component.extend({
       var params = {
         query: this.get('query'),
         author: this.get('author'),
-        notes: this.get('notes')
+        notes: this.get('notes'),
+        timestamp: this.get('timestamp') ? this.get('timestamp') : moment().format('MMMM Do YYYY, h:mm a')
       };
       this.sendAction('save', params);
     }
