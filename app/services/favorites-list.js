902a9c747f88ac21df30832ae21f54768e8d637d
Anna Becker <anna.r.becker@gmail.com>
2016-04-07 18:34:49 -0700

    Add favorites-list service so user can bookmark their favorite questions

---
+++ app/services/favorites-list.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Service.extend({
+  questions: [],
+
+  add(question) {
+    this.get('questions').pushObject(question);
+  },
+  remove(question) {
+    this.get('questions').removeObject(question)
+  },
+  includes('question') {
+    return this.get('questions').includes(question);
+  },
+  empty() {
+    this.get('questions').setObjects([]);
+  }
+});
